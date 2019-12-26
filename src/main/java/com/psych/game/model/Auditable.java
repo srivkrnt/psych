c4767e454f1b0e4ef3fb961a108113112fa4d976
Vikrant <vikrant@radicali.io>
2019-12-26 22:31:08 +0530

    Rest API - Players

---
+++ src/main/java/com/psych/game/model/Auditable.java
@@ -0,0 +1,26 @@
+package com.psych.game.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(allowGetters = true)
+public abstract class Auditable implements Serializable {
+    @Column(nullable = false,updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt = new Date();
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt = new Date();
+}
